27f48f644039baddabf4de9a0f168779a81e0305
Hakim El Hattab <hakim.elhattab@gmail.com>
2012-04-25 23:24:29 -0400

    significantly smaller js file header, added minified js

--- js/reveal.js
+++ js/reveal.js
@@ -1,42 +1,9 @@
-/**
+/*!
+ * reveal.js 1.3
+ * http://lab.hakim.se/reveal-js
+ * MIT licensed
+ * 
  * Copyright (C) 2011 Hakim El Hattab, http://hakim.se
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- * 
- * #############################################################################
- *
- * Reveal.js is an easy to use HTML based slideshow enhanced by 
- * sexy CSS 3D transforms.
- * 
- * Slides are given unique hash based URL's so that they can be 
- * opened directly.
- * 
- * Public facing methods:
- * - Reveal.initialize( { ... options ... } );
- * - Reveal.navigateTo( indexh, indexv );
- * - Reveal.navigateLeft();
- * - Reveal.navigateRight();
- * - Reveal.navigateUp();
- * - Reveal.navigateDown();
- * 	
- * @author Hakim El Hattab | http://hakim.se
- * @version 1.3
  */
 var Reveal = (function(){
 	
