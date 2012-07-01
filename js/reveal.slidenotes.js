9608ac9d2dbe7ef4161d64c88865290ccaca6c30
Hakim El Hattab <hakim.elhattab@gmail.com>
2012-07-01 16:43:43 -0400

    adjust whitespace of notes code to match rest of project

--- js/reveal.slidenotes.js
+++ js/reveal.slidenotes.js
@@ -1,35 +1,35 @@
 (function() {
-  // don't emit events from inside the previews themselves
-  var qs = window.location.href.split('?');
-  if (qs.length > 1 && qs[1].match('receiver')) { return; }
+	// don't emit events from inside the previews themselves
+	var qs = window.location.href.split('?');
+	if (qs.length > 1 && qs[1].match('receiver')) { return; }
 
-  var socket = io.connect(window.location.origin);
-  var socketId = Math.random().toString().slice(2);
-  console.log('View slide notes at ' + window.location.origin + '/_notes/' + socketId);
+	var socket = io.connect(window.location.origin);
+	var socketId = Math.random().toString().slice(2);
+	console.log('View slide notes at ' + window.location.origin + '/_notes/' + socketId);
 
-  Reveal.addEventListener( 'slidechanged', function( event ) {
-    var nextindexh;
-    var nextindexv;
-    var slideElement = event.currentSlide;
+	Reveal.addEventListener( 'slidechanged', function( event ) {
+		var nextindexh;
+		var nextindexv;
+		var slideElement = event.currentSlide;
 
-    if (slideElement.nextElementSibling && slideElement.parentNode.nodeName == 'SECTION') {
-      nextindexh = event.indexh;
-      nextindexv = event.indexv + 1;
-    } else {
-      nextindexh = event.indexh + 1;
-      nextindexv = 0;
-    }
+		if (slideElement.nextElementSibling && slideElement.parentNode.nodeName == 'SECTION') {
+			nextindexh = event.indexh;
+			nextindexv = event.indexv + 1;
+		} else {
+			nextindexh = event.indexh + 1;
+			nextindexv = 0;
+		}
 
-    var notes = slideElement.querySelector('aside.notes');
-    var slideData = {
-      notes : notes ? notes.innerHTML : '',
-      indexh : event.indexh,
-      indexv : event.indexv,
-      nextindexh : nextindexh,
-      nextindexv : nextindexv,
-      socketId : socketId
-    };
+		var notes = slideElement.querySelector('aside.notes');
+		var slideData = {
+			notes : notes ? notes.innerHTML : '',
+			indexh : event.indexh,
+			indexv : event.indexv,
+			nextindexh : nextindexh,
+			nextindexv : nextindexv,
+			socketId : socketId
+		};
 
-    socket.emit('slidechanged', slideData);
-  } );
+		socket.emit('slidechanged', slideData);
+	} );
 }());
