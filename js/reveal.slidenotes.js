0e8640094301889e5c848dcf92084678778f0a19
Hakim El Hattab <hakim.elhattab@gmail.com>
2012-07-04 14:18:09 -0400

    conditional loading of socket.io and the notes client js based on window host

--- js/reveal.slidenotes.js
+++ js/reveal.slidenotes.js
@@ -5,7 +5,6 @@
 
 	var socket = io.connect(window.location.origin);
 	var socketId = Math.random().toString().slice(2);
-	
 	console.log('View slide notes at ' + window.location.origin + '/notes/' + socketId);
 
 	Reveal.addEventListener( 'slidechanged', function( event ) {
