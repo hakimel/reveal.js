7cf17ac0633b6082d118f5bad9d6201510c040dc
David Banham <david@banham.id.au>
2012-08-06 14:40:32 +1000

    Removed unused code

--- plugin/multiplex/index.js
+++ plugin/multiplex/index.js
@@ -1,8 +1,6 @@
 var express   = require('express');
 var fs        = require('fs');
 var io        = require('socket.io');
-var _         = require('underscore');
-var Mustache  = require('mustache');
 var crypto		= require('crypto');
 
 var app       = express.createServer();
@@ -49,14 +47,3 @@ var createHash = function(secret) {
 
 // Actually listen
 app.listen(opts.port || null);
-
-var brown = '\033[33m', 
-	green = '\033[32m', 
-	reset = '\033[0m';
-
-var slidesLocation = "http://localhost" + ( opts.port ? ( ':' + opts.port ) : '' );
-
-console.log( brown + "reveal.js - Speaker Notes" + reset );
-console.log( "1. Open the slides at " + green + slidesLocation + reset );
-console.log( "2. Click on the link your JS console to go to the notes page" );
-console.log( "3. Advance through your slides and your notes will advance automatically" );
