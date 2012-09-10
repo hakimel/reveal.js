edfcfb1d3f836375831a8db93f8c31d3cd617bfe
Hakim El Hattab <hakim.elhattab@gmail.com>
2012-09-09 22:17:32 -0400

    open slide notes window automatically #135

--- plugin/speakernotes/client.js
+++ plugin/speakernotes/client.js
@@ -5,7 +5,8 @@
 	var socket = io.connect(window.location.origin);
 	var socketId = Math.random().toString().slice(2);
 	
-        window.open(window.location.origin + '/notes/' + socketId, 'notes-' + socketId)
+	console.log('View slide notes at ' + window.location.origin + '/notes/' + socketId);
+	window.open(window.location.origin + '/notes/' + socketId, 'notes-' + socketId)
 
 	Reveal.addEventListener( 'slidechanged', function( event ) {
 		var nextindexh;
