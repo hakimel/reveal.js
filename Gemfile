d687945dd66d7b03b50e4a08180ec15aceb5bf2b
Chris Moore <cdmwebs@gmail.com>
2012-10-16 19:23:03 -0400

    setup compass, sass and guard
    * add a Guardfile to watch for changes
    * add Sass and Compass
    * vendor everything and ignore relevant folders
    * use guard-compass for watching

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+ruby '1.9.3'
+
+gem 'compass'
+gem 'guard'
+gem 'guard-compass', require: false
+gem 'rb-fsevent', '~> 0.9.1'
+gem 'terminal-notifier-guard'
+
---
+++ config.rb
@@ -0,0 +1,19 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+preferred_syntax = :sass
