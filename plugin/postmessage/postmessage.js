87591d95d18b9fe215f06791bd639a6ae4839cfd
Hakim El Hattab <hakim.elhattab@gmail.com>
2012-11-10 17:15:11 -0500

    cosmetical tweaks to postmessage plugin

--- plugin/postmessage/postmessage.js
+++ plugin/postmessage/postmessage.js
@@ -1,39 +1,41 @@
 /*
-simple postmessage plugin
 
-Useful when a reveal slideshow is inside an iframe.
-It allows to call reveal methods from outside.
+	simple postmessage plugin
 
-Example:
-   var reveal =  window.frames[0];
+	Useful when a reveal slideshow is inside an iframe.
+	It allows to call reveal methods from outside.
 
-   // Reveal.prev(); 
-   reveal.postMessage(JSON.stringify({method: 'prev', args: []}), '*');
-   // Reveal.next(); 
-   reveal.postMessage(JSON.stringify({method: 'next', args: []}), '*');
-   // Reveal.slide(2, 2); 
-   reveal.postMessage(JSON.stringify({method: 'slide', args: [2,2]}), '*');
+	Example:
+		 var reveal =  window.frames[0];
 
-Add to the slideshow:
+		 // Reveal.prev(); 
+		 reveal.postMessage(JSON.stringify({method: 'prev', args: []}), '*');
+		 // Reveal.next(); 
+		 reveal.postMessage(JSON.stringify({method: 'next', args: []}), '*');
+		 // Reveal.slide(2, 2); 
+		 reveal.postMessage(JSON.stringify({method: 'slide', args: [2,2]}), '*');
 
-	dependencies: [
-        ...
-		{ src: 'plugin/postmessage/postmessage.js', async: true, condition: function() { return !!document.body.classList; } }
-	]
+	Add to the slideshow:
 
+		dependencies: [
+			...
+			{ src: 'plugin/postmessage/postmessage.js', async: true, condition: function() { return !!document.body.classList; } }
+		]
 
 */
 
 (function (){
 
-window.addEventListener("message", function (event){
-    var data = JSON.parse(event.data),
-        method = data.method,
-        args = data.args;
-    if (Reveal[method]){
-        Reveal[method].apply(Reveal, data.args);
-    }
-}, false);
+	window.addEventListener( "message", function ( event ) {
+		var data = JSON.parse( event.data ),
+				method = data.method,
+				args = data.args;
+
+		if( typeof Reveal[method] === 'function' ) {
+			Reveal[method].apply( Reveal, data.args );
+		}
+	}, false);
+
 }());
 
 
