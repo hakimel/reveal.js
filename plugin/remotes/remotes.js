b9483f29e142ccfc681367014d8ef51813e98667
callmephilip <philip.nuzhnyy@gmail.com>
2013-01-04 17:12:20 -0800

    disable remotes plugin on mobile

--- plugin/remotes/remotes.js
+++ plugin/remotes/remotes.js
@@ -3,17 +3,28 @@
  * of the folks at http://remotes.io
  */
 
-head.ready( 'remotes.ne.min.js', function() {
-	
-	new Remotes("preview")
-		.on("swipe-left", function(e){ Reveal.right(); })
-		.on("swipe-right", function(e){ Reveal.left(); })
-		.on("swipe-up", function(e){ Reveal.down(); })
-		.on("swipe-down", function(e){ Reveal.up(); })
-		.on("tap", function(e){ 
-			Reveal.toggleOverview(); 
-		});
+(function(window){
 
-} );
+    /**
+     * Detects if we are dealing with a touch enabled device (with some false positives)
+     * Borrowed from modernizr: https://github.com/Modernizr/Modernizr/blob/master/feature-detects/touch.js   
+     */
+    var hasTouch  = (function(){
+        return ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch;
+    })();
 
-head.js( 'https://raw.github.com/Remotes/Remotes/master/dist/remotes.ne.min.js' );
+    if(!hasTouch){
+        head.ready( 'remotes.ne.min.js', function() {
+            new Remotes("preview")
+                .on("swipe-left", function(e){ Reveal.right(); })
+                .on("swipe-right", function(e){ Reveal.left(); })
+                .on("swipe-up", function(e){ Reveal.down(); })
+                .on("swipe-down", function(e){ Reveal.up(); })
+                .on("tap", function(e){ 
+                    Reveal.toggleOverview(); 
+                });
+        } );
+
+        head.js('https://raw.github.com/Remotes/Remotes/master/dist/remotes.ne.min.js');
+    }
+})(window);
