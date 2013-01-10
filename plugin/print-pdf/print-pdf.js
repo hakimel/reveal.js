05b5255d6046d7b87a691f8f7ea8a397f60a0eaa
Hakim El Hattab <hakim.elhattab@gmail.com>
2013-01-10 09:40:03 -0500

    attribution for print-pdf phantom script (closes #276)

--- plugin/print-pdf/print-pdf.js
+++ plugin/print-pdf/print-pdf.js
@@ -2,8 +2,9 @@
  * phantomjs script for printing presentations to PDF.
  *
  * Example:
- *
  * phantomjs print-pdf.js "http://lab.hakim.se/reveal-js?print-pdf" reveal-demo.pdf
+ *
+ * By Manuel Bieh (https://github.com/manuelbieh)
  */
 
 // html2pdf.js
