d4b73503d88b4f1b2c742b509d46f0d59b30bb8f
Christoph Wagner <cwagner@oversee.net>
2013-03-13 16:42:02 -0700

    Adding grunt-contrib-jade

--- Gruntfile.js
+++ Gruntfile.js
@@ -94,6 +94,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 	grunt.loadNpmTasks( 'grunt-contrib-sass' );
+	grunt.loadNpmTasks( 'grunt-contrib-jade' );
 
 	// Default task
 	grunt.registerTask( 'default', [ 'jshint', 'cssmin', 'uglify' ] );
