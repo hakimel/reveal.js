1748a29ff3b7cbbe72616d82f00063ac164942f9
Hakim El Hattab <hakim.elhattab@gmail.com>
2013-08-12 09:21:38 -0400

    first version of mathjax plugin #531

---
+++ plugin/math/math.js
@@ -0,0 +1,37 @@
+/**
+ * A plugin which enables rendering of math equations inside
+ * of reveal.js slides. Essentially a thin wrapper for MathJax.
+ *
+ * @author Hakim El Hattab
+ */
+(function(){
+
+	var head = document.querySelector( 'head' );
+	var script = document.createElement( 'script' );
+	script.type = 'text/javascript';
+	script.src = 'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_SVG-full';
+
+	// Detect when the script has loaded
+	script.onload = onScriptLoad;
+	script.onreadystatechange = function() {
+		if ( this.readyState === 'loaded' ) {
+			onScriptLoad.call();
+		}
+	}
+
+	head.appendChild( script );
+
+	function onScriptLoad() {
+
+		MathJax.Hub.Config({
+			messageStyle: 'none',
+			tex2jax: { inlineMath: [['$','$'],['\\(','\\)']] }
+		});
+
+		Reveal.addEventListener( 'slidechanged', function( event ) {
+			MathJax.Hub.Rerender();
+		} );
+
+	}
+
+})();
