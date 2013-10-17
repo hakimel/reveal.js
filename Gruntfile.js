f50d6a836a7e2fa61498d531fb915d789b7afec0
marcysutton <holla@marcysutton.com>
2013-10-17 16:11:26 -0700

    Theming project

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,14 +41,14 @@ module.exports = function(grunt) {
 		sass: {
 			main: {
 				files: {
+					'css/theme/marcysutton.css': 'css/theme/source/marcysutton.scss',
 					'css/theme/default.css': 'css/theme/source/default.scss',
 					'css/theme/beige.css': 'css/theme/source/beige.scss',
 					'css/theme/night.css': 'css/theme/source/night.scss',
 					'css/theme/serif.css': 'css/theme/source/serif.scss',
 					'css/theme/simple.css': 'css/theme/source/simple.scss',
 					'css/theme/sky.css': 'css/theme/source/sky.scss',
-					'css/theme/moon.css': 'css/theme/source/moon.scss',
-					'css/theme/solarized.css': 'css/theme/source/solarized.scss'
+					'css/theme/moon.css': 'css/theme/source/moon.scss'
 				}
 			}
 		},
