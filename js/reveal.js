44e6f7ace070401801a50ad6cf47a8dab8d933da
Armand Abric <contact@forgebinaire.net>
2014-02-07 19:03:48 +0100

    Increase Logitech R400 remote compatibity.

--- js/reveal.js
+++ js/reveal.js
@@ -2773,8 +2773,8 @@ var Reveal = (function(){
 				case 32: isOverview() ? deactivateOverview() : event.shiftKey ? navigatePrev() : navigateNext(); break;
 				// return
 				case 13: isOverview() ? deactivateOverview() : triggered = false; break;
-				// b, period, Logitech presenter tools "black screen" button
-				case 66: case 190: case 191: togglePause(); break;
+				// two-spot, semicolon, b, period, Logitech presenter tools "black screen" button
+				case 58: case: 59: case 66: case 190: case 191: togglePause(); break;
 				// f
 				case 70: enterFullscreen(); break;
 				// a
