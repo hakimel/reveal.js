13997f80035dc48445a1d8e508887074c154b519
Ray Villalobos <ray@planetoftheweb.com>
2014-03-24 19:19:47 -0400

    Created a template based on .scss files

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,8 @@ module.exports = function(grunt) {
 					'css/theme/sky.css': 'css/theme/source/sky.scss',
 					'css/theme/moon.css': 'css/theme/source/moon.scss',
 					'css/theme/solarized.css': 'css/theme/source/solarized.scss',
-					'css/theme/blood.css': 'css/theme/source/blood.scss'
+					'css/theme/blood.css': 'css/theme/source/blood.scss',
+					'css/theme/ray.css': 'css/theme/source/ray.scss'
 				}
 			}
 		},
