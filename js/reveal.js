5e85f02eb1596d0c5a71aa35430c0914e0b6d35a
Hakim El Hattab <hakim.elhattab@gmail.com>
2014-05-04 09:32:10 +0200

    ensure default can be prevented

--- js/reveal.js
+++ js/reveal.js
@@ -3278,7 +3278,7 @@
 		// If the input resulted in a triggered action we should prevent
 		// the browsers default behavior
 		if( triggered ) {
-			event.preventDefault();
+			event.preventDefault && event.preventDefault();
 		}
 		// ESC or O key
 		else if ( ( event.keyCode === 27 || event.keyCode === 79 ) && features.transforms3d ) {
@@ -3289,7 +3289,7 @@
 				toggleOverview();
 			}
 
-			event.preventDefault();
+			event.preventDefault && event.preventDefault();
 		}
 
 		// If auto-sliding is enabled we need to cue up
