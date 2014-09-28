9ef0060024a428e2bbfa237f2632e2defe4a5ec3
Hakim El Hattab <hakim.elhattab@gmail.com>
2014-09-28 11:17:16 +0200

    add new black theme

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,6 +41,7 @@ module.exports = function(grunt) {
 			main: {
 				files: {
 					'css/theme/default.css': 'css/theme/source/default.scss',
+					'css/theme/black.css': 'css/theme/source/black.scss',
 					'css/theme/beige.css': 'css/theme/source/beige.scss',
 					'css/theme/night.css': 'css/theme/source/night.scss',
 					'css/theme/serif.css': 'css/theme/source/serif.scss',
