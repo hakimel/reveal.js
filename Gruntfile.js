bf8a6271d4dfc7cd6e68e01583c927b5f5730d62
Hakim El Hattab <hakim.elhattab@gmail.com>
2015-02-25 15:35:30 +0100

    fix mixed indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -92,8 +92,8 @@ module.exports = function(grunt) {
 				options: {
 					port: port,
 					base: '.',
-                    livereload: true,
-                    open: true
+					livereload: true,
+					open: true
 				}
 			}
 		},
@@ -110,9 +110,9 @@ module.exports = function(grunt) {
 		},
 
 		watch: {
-            options: {
-                livereload: true
-            },
+			options: {
+				livereload: true
+			},
 			js: {
 				files: [ 'Gruntfile.js', 'js/reveal.js' ],
 				tasks: 'js'
@@ -125,9 +125,9 @@ module.exports = function(grunt) {
 				files: [ 'css/reveal.scss' ],
 				tasks: 'css-core'
 			},
-            html: {
-                files: [ 'index.html']
-            }
+			html: {
+				files: [ 'index.html']
+			}
 		}
 
 	});
