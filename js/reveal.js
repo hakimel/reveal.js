f772c7eb508cb1373d509d8cfea3b89984290ae9
Hakim El Hattab <hakim.elhattab@gmail.com>
2015-02-25 13:31:41 +0100

    fix progress bar clicks in rtl mode #1131

--- js/reveal.js
+++ js/reveal.js
@@ -4001,6 +4001,10 @@
 		var slidesTotal = toArray( dom.wrapper.querySelectorAll( HORIZONTAL_SLIDES_SELECTOR ) ).length;
 		var slideIndex = Math.floor( ( event.clientX / dom.wrapper.offsetWidth ) * slidesTotal );
 
+		if( config.rtl ) {
+			slideIndex = slidesTotal - slideIndex;
+		}
+
 		slide( slideIndex );
 
 	}
