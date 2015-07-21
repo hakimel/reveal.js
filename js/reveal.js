b7470fa3239bca8858ee215adf59118ed12fa581
teawithfruit <teawithfruit@gmail.com>
2015-07-21 15:59:19 +0200

    solves early access error with video element
    This will maybe solve the "InvalidStateError: An attempt was made to use an object that is not, or is no longer, usable" error in firefox.

--- js/reveal.js
+++ js/reveal.js
@@ -2612,7 +2612,7 @@
 			// Start video playback
 			var currentVideo = currentBackground.querySelector( 'video' );
 			if( currentVideo ) {
-				currentVideo.currentTime = 0;
+				if(currentVideo.currentTime > 0) currentVideo.currentTime = 0;
 				currentVideo.play();
 			}
 
