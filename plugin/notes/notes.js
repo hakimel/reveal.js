9bcd1484bc583ffe015c5296e6a42c17f578ae49
Hakim El Hattab <hakim.elhattab@gmail.com>
2015-09-25 09:41:05 +0200

    notes defined in data-notes attributes retain preformatted whitespace

--- plugin/notes/notes.js
+++ plugin/notes/notes.js
@@ -55,12 +55,14 @@ var RevealNotes = (function() {
 				type: 'state',
 				notes: '',
 				markdown: false,
+				whitespace: 'normal',
 				state: Reveal.getState()
 			};
 
 			// Look for notes defined in a slide attribute
 			if( slideElement.hasAttribute( 'data-notes' ) ) {
 				messageData.notes = slideElement.getAttribute( 'data-notes' );
+				messageData.whitespace = 'pre-wrap';
 			}
 
 			// Look for notes defined in an aside element
