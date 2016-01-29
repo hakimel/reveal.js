47892cb3531138cdab665cb4e8d8f5c7cf44f0db
Michel VACHER <michel.vacher@soat.fr>
2016-01-29 11:06:15 +0100

    add custom renderer to markdown plugin

--- plugin/markdown/markdown.js
+++ plugin/markdown/markdown.js
@@ -349,7 +349,8 @@
 	 * DOM to HTML.
 	 */
 	function convertSlides() {
-
+		var renderer = typeof Reveal.getConfig()['markdown_renderer'] == 'function' ? Reveal.getConfig()['markdown_renderer']() : new marked.Renderer();
+	
 		var sections = document.querySelectorAll( '[data-markdown]');
 
 		for( var i = 0, len = sections.length; i < len; i++ ) {
