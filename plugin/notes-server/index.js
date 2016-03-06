9eb56f8146e9d4d57e579fcc1a40ae370b036e5b
Benjamin Tan <demoneaux@gmail.com>
2016-03-06 16:52:02 +0800

    Remove unused dependencies.

--- plugin/notes-server/index.js
+++ plugin/notes-server/index.js
@@ -2,7 +2,6 @@ var http      = require('http');
 var express   = require('express');
 var fs        = require('fs');
 var io        = require('socket.io');
-var _         = require('underscore');
 var Mustache  = require('mustache');
 
 var app       = express();
@@ -64,5 +63,5 @@ var slidesLocation = 'http://localhost' + ( opts.port ? ( ':' + opts.port ) : ''
 
 console.log( brown + 'reveal.js - Speaker Notes' + reset );
 console.log( '1. Open the slides at ' + green + slidesLocation + reset );
-console.log( '2. Click on the link your JS console to go to the notes page' );
+console.log( '2. Click on the link in your JS console to go to the notes page' );
 console.log( '3. Advance through your slides and your notes will advance automatically' );
