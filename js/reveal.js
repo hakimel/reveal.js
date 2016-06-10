8162855d23601e66a25ba79dd4013ef416c4f1b4
Hakim El Hattab <hakim.elhattab@gmail.com>
2016-06-10 10:58:40 +0200

    increase default font sizes, adjust config defaults for less margings and larger max scale #1605

--- js/reveal.js
+++ js/reveal.js
@@ -43,11 +43,11 @@
 			height: 700,
 
 			// Factor of the display size that should remain empty around the content
-			margin: 0.1,
+			margin: 0.04,
 
 			// Bounds for smallest/largest possible scale to apply to content
 			minScale: 0.2,
-			maxScale: 1.5,
+			maxScale: 2.0,
 
 			// Display controls in the bottom right corner
 			controls: true,
