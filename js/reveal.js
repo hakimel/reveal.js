5a76227de09cd9a3018b4f8b73e3e30d6ba0b295
Jan Beilicke <dev@jotbe.io>
2016-10-29 18:15:52 +0200

    Fix: Background image was not displayed on very first title slide

--- js/reveal.js
+++ js/reveal.js
@@ -2971,13 +2971,6 @@
 		// Hide the slide element
 		slide.style.display = 'none';
 
-		// Hide the corresponding background element
-		var indices = getIndices( slide );
-		var background = getSlideBackground( indices.h, indices.v );
-		if( background ) {
-			background.style.display = 'none';
-		}
-
 	}
 
 	/**
