2bc49db5c84cb5dcb66cecbc82f5ffbbacb320ba
kadevgraaf <kadegraaf@gmail.com>
2016-12-08 15:59:37 +0100

    SWIK 789 Bottom of slide content is missing in presentation mode
    Fixed line-height + default slide height

--- js/reveal.js
+++ js/reveal.js
@@ -40,7 +40,7 @@
 			// The "normal" size of the presentation, aspect ratio will be preserved
 			// when the presentation is scaled to fit different resolutions
 			width: 960,
-			height: 700,
+			height: 720,
 
 			// Factor of the display size that should remain empty around the content
 			margin: 0.1,
