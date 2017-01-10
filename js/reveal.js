fb8414f4cb092a9f675e783b29b651a03a7858f4
Hakim El Hattab <hakim.elhattab@gmail.com>
2017-01-10 13:47:24 +0100

    3.4.0

--- js/reveal.js
+++ js/reveal.js
@@ -26,7 +26,7 @@
 	var Reveal;
 
 	// The reveal.js version
-	var VERSION = '3.3.0';
+	var VERSION = '3.4.0';
 
 	var SLIDES_SELECTOR = '.slides section',
 		HORIZONTAL_SLIDES_SELECTOR = '.slides>section',
