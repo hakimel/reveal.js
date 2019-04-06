3ff070536e4ed84d7c880bd91edd52d98945f1b6
Richard Westenra <rjwestenra@gmail.com>
2019-04-06 11:07:05 +0300

    Remove calls to missing modules from gruntfile
    When running the app, Grunt throws the following warnings:
    
    >> Local Npm module "grunt-contrib-clean" not found. Is it installed?
    >> Local Npm module "grunt-contrib-nodeunit" not found. Is it installed?
    
    These modules don't appear to do anything except throw a warning so they can probably just be removed.

--- gruntfile.js
+++ gruntfile.js
@@ -162,9 +162,6 @@ module.exports = grunt => {
 
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
 	// Default task
 	grunt.registerTask( 'default', [ 'css', 'js' ] );
 
