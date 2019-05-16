7ef1d60852519906582f141e9e89d8af7ddb42b3
Zach DeCook <you@example.com>
2019-05-16 13:07:37 -0400

    * Dimensions: Allow mixing percent-based and absolute sizes
    Allow fixed size to be set as a string

--- js/reveal.js
+++ js/reveal.js
@@ -2400,13 +2400,23 @@
 		size.presentationHeight -= ( size.presentationHeight * config.margin );
 
 		// Slide width may be a percentage of available width
-		if( typeof size.width === 'string' && /%$/.test( size.width ) ) {
+		if( typeof config.width === 'string' && /%$/.test( config.width ) ) {
 			size.width = parseInt( size.width, 10 ) / 100 * size.presentationWidth;
+			// If width was a percentage, and height was a value,
+			if( typeof config.height !== 'string' || ! /%$/.test( config.height ) ) {
+				// scale the width by the width scale, so actual size is correct.
+				size.width = ( size.height / size.presentationHeight ) * size.width;
+			}
 		}
 
 		// Slide height may be a percentage of available height
-		if( typeof size.height === 'string' && /%$/.test( size.height ) ) {
+		if( typeof config.height === 'string' && /%$/.test( config.height ) ) {
 			size.height = parseInt( size.height, 10 ) / 100 * size.presentationHeight;
+			// If height was a percentage, and width was a value,
+			if( typeof config.width !== 'string' || ! /%$/.test( config.width ) ) {
+				// scale the height by the width scale, so actual size is correct.
+				size.height = ( size.width / size.presentationWidth ) * size.height;
+			}
 		}
 
 		return size;
