7cdf50822d7f717c69bdea639ba4d6a65ce149a3
Pedro Cunial <pedro.campos@quintoandar.com.br>
2019-10-29 08:04:02 -0300

    Refactored redundant if + ternary statements
    Changed it into a single ternary (since the "false" option of the ternary would never happen).

--- js/reveal.js
+++ js/reveal.js
@@ -4472,12 +4472,10 @@
 
 		var horizontalSlide = dom.wrapper.querySelectorAll( HORIZONTAL_SLIDES_SELECTOR )[ x ];
 		var verticalSlides = horizontalSlide && horizontalSlide.querySelectorAll( 'section' );
+		var hasVerticalSlides = verticalSlides && verticalSlides.length && typeof y === 'number';
+		
+		return hasVerticalSlides ? verticalSlides[ y ] : horizontalSlide;
 
-		if( verticalSlides && verticalSlides.length && typeof y === 'number' ) {
-			return verticalSlides ? verticalSlides[ y ] : undefined;
-		}
-
-		return horizontalSlide;
 
 	}
 
