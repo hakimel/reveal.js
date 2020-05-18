fb22716ddf9f369371922ce67526d1fad9a92ae9
Hakim El Hattab <hakim.elhattab@gmail.com>
2020-05-18 14:41:56 +0200

    move built plugin files from /dist/plugin/ to /plugin/<name>/

--- gulpfile.js
+++ gulpfile.js
@@ -108,12 +108,12 @@ gulp.task('js', gulp.parallel('js-es5', 'js-es6'));
 // built-in plugins
 gulp.task('plugins', () => {
     return Promise.all([
-        { name: 'RevealHighlight', input: './plugin/highlight/plugin.js', output: './dist/plugin/highlight' },
-        { name: 'RevealMarkdown', input: './plugin/markdown/plugin.js', output: './dist/plugin/markdown' },
-        { name: 'RevealSearch', input: './plugin/search/plugin.js', output: './dist/plugin/search' },
-        { name: 'RevealNotes', input: './plugin/notes/plugin.js', output: './dist/plugin/notes' },
-        { name: 'RevealZoom', input: './plugin/zoom/plugin.js', output: './dist/plugin/zoom' },
-        { name: 'RevealMath', input: './plugin/math/plugin.js', output: './dist/plugin/math' },
+        { name: 'RevealHighlight', input: './plugin/highlight/plugin.js', output: './plugin/highlight/highlight' },
+        { name: 'RevealMarkdown', input: './plugin/markdown/plugin.js', output: './plugin/markdown/markdown' },
+        { name: 'RevealSearch', input: './plugin/search/plugin.js', output: './plugin/search/search' },
+        { name: 'RevealNotes', input: './plugin/notes/plugin.js', output: './plugin/notes/notes' },
+        { name: 'RevealZoom', input: './plugin/zoom/plugin.js', output: './plugin/zoom/zoom' },
+        { name: 'RevealMath', input: './plugin/math/plugin.js', output: './plugin/math/math' },
     ].map( plugin => {
         return rollup({
                 input: plugin.input,
@@ -248,7 +248,7 @@ gulp.task('serve', () => {
 
     gulp.watch(['js/**'], gulp.series('js', 'test'))
 
-    gulp.watch(['plugin/**/*.js'], gulp.series('plugins'))
+    gulp.watch(['plugin/**/plugin.js'], gulp.series('plugins'))
 
     gulp.watch(['test/*.html'], gulp.series('test'))
 
