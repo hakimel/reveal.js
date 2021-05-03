67f1f68ef16d6fb87fca291d4a74388587f574c4
Sean Killeen <SeanKilleen@gmail.com>
2021-05-03 13:55:21 -0400

    Correct two minor typos

--- js/config.js
+++ js/config.js
@@ -73,8 +73,8 @@ export default {
 
 	// Optional function that blocks keyboard events when retuning false
 	//
-	// If you set this to 'foucsed', we will only capture keyboard events
-	// for embdedded decks when they are in focus
+	// If you set this to 'focused', we will only capture keyboard events
+	// for embedded decks when they are in focus
 	keyboardCondition: null,
 
 	// Disables the default reveal.js slide layout (scaling and centering)
