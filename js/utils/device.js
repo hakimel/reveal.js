80d0b08429455ca728734143605528fbc1113cdc
SKTT1Ryze <SKTT1Ryze@users.noreply.github.com>
2022-05-25 03:41:23 +0000

    Automated deployment: Wed May 25 03:41:23 UTC 2022 7481b674128e55b8a2131d98154a36a6869fee2b

--- js/utils/device.js
+++
@@ -1,8 +0,0 @@
-const UA = navigator.userAgent;
-
-export const isMobile = /(iphone|ipod|ipad|android)/gi.test( UA ) ||
-						( navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1 ); // iPadOS
-
-export const isChrome = /chrome/i.test( UA ) && !/edge/i.test( UA );
-
-export const isAndroid = /android/gi.test( UA );
--- test/assets/external-script-b.js
+++
@@ -1,1 +0,0 @@
-window.externalScriptSequence += 'B';
