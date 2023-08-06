92ee97fbfe74239e8bc196b0954bd19a23410fd4
Hakim El Hattab <hakim.elhattab@gmail.com>
2023-08-06 14:02:13 +0200

    update markdown default notes separator to ignore inline occurances of 'notes:', closes #1915, closes #2762

--- plugin/markdown/plugin.js
+++ plugin/markdown/plugin.js
@@ -8,7 +8,7 @@ import { marked } from 'marked';
 
 const DEFAULT_SLIDE_SEPARATOR = '\r?\n---\r?\n',
 	  DEFAULT_VERTICAL_SEPARATOR = null,
-	  DEFAULT_NOTES_SEPARATOR = 'notes?:',
+	  DEFAULT_NOTES_SEPARATOR = '^\s*notes?:',
 	  DEFAULT_ELEMENT_ATTRIBUTES_SEPARATOR = '\\\.element\\\s*?(.+?)$',
 	  DEFAULT_SLIDE_ATTRIBUTES_SEPARATOR = '\\\.slide:\\\s*?(\\\S.+?)$';
 
